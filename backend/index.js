5d6892484ae5f37a99354899d170a1ed37c6f0ab
Aris <frestaris@gmail.com>
2024-11-26 15:09:41 +1000

    backend installed

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app = express();
+
+mongoose
+  .connect(process.env.MONGO_DB)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+// Middlewares
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ limit: "25mb", extended: true }));
+app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
